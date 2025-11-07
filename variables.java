b188923fe766b4effe4c28e26eefdbec74a79d23
buddha02102058 <bibekgiri925@gmail.com>
2025-11-07 22:48:38 +0545

    learned about variables in java

---
+++ variables.java
@@ -0,0 +1,35 @@
+public class variables {
+    public static void main(String[] args){
+
+        //Data types that stores numbers
+        //byte = 1 byte
+        //short = 2 bytes
+        //int = 4 bytes
+        //long = 8 bytes
+
+        //Data types that stores decimal numbers
+        //float = 4 bytes   
+        //double = 8 bytes
+
+        //Data type that stores a single character
+        //char = 2 bytes
+
+        //Data type that stores true or false values
+        //boolean = 1 bit
+
+
+        int x = 10; // integer variable
+        float Pi=(float)3.14; // float variable
+        double b=3.14159265359; // double variable
+        char Letter='S'; // char variable
+        boolean isJavaFun=true; // boolean variable
+
+      System.out.println(x); 
+      System.out.println(Pi); 
+      System.out.println(b); 
+      System.out.println(Letter); 
+      System.out.println(isJavaFun);  
+
+    }
+    
+}
